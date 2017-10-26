67b5b3f4a933edbcf65142badded22bb586cf7b6
jaroslaw <jarek.skrzypczak@o2.pl>
2017-10-26 09:37:38 +0200

    Refactor 3 method. Now are shorter and readable.

--- Lab_3/Zad_3.py
+++ Lab_3/Zad_3.py
@@ -54,69 +54,26 @@ def gradient_rgb_bw(v):
 
 
 def gradient_rgb_gbr(v):
-    r = 0
-    g = 0
-    if (v <= 0.5):
-        g = 1.0 - v * 2
-        b = v * 2
-    else:
-        r = v * 2 - 1
-        b = 2.0 - v * 2
+    pieces = 2
+    colors = [[0, 1, 0], [0, 0, 1], [1, 0, 0]]
+    i = int(np.floor(pieces * v)) % pieces
+    r, g, b = inte(colors[i], colors[i + 1], i + 1, pieces, v)
     return (r, g, b)
 
 
 def gradient_rgb_gbr_full(v):
-    r = 0
-    g = 0
-    if (v <= 0.25):
-        g = 1
-        b = v * 4
-    elif (v <= 0.5):
-        b = 1
-        g = 1.0 - v * 4 + 1
-    elif (v <= 0.75):
-        b = 1
-        r = v * 4 - 2
-    else:
-        r = 1
-        b = 4.0 - 4 * v
+    pieces = 4
+    colors = [[0, 1, 0], [0, 1, 1], [0, 0, 1], [1, 0, 1], [1, 0, 0]]
+    i = int(np.floor(pieces * v)) % pieces
+    r, g, b = inte(colors[i], colors[i + 1], i + 1, pieces, v)
     return (r, g, b)
-
-
-def calcValue(entry, out, piece, all, value):
-    if (entry > out):
-        return piece - all * value
-    else:
-        return all * value - piece + 1
-
-
-def inte(of, to, piece, all, value):
-    a = of[0]
-    d = of[1]
-    c = of[2]
-    x = to[0]
-    y = to[1]
-    z = to[2]
-    if (a != x):
-        r = calcValue(a, x, piece, all, value)
-    else:
-        r = a
-    if (d != y):
-        g = calcValue(d, y, piece, all, value)
-    else:
-        g = d
-    if (c != z):
-        b = calcValue(c, z, piece, all, value)
-    else:
-        b = c
-    return r, g, b
 
 
 def gradient_rgb_wb_custom(v):
     pieces = 7
     colors = [[1, 1, 1], [1, 0, 1], [0, 0, 1], [0, 1, 1], [0, 1, 0], [1, 1, 0], [1, 0, 0], [0, 0, 0]]
-    i = (int) (np.floor(pieces*v))%7
-    r, g, b = inte(colors[i], colors[i + 1], i+1, pieces, v)
+    i = int(np.floor(pieces * v)) % pieces
+    r, g, b = inte(colors[i], colors[i + 1], i + 1, pieces, v)
     return (r, g, b)
 
 
@@ -140,6 +97,31 @@ def gradient_hsv_unknown(v):
 
 def gradient_hsv_custom(v):
     return cs.hsv_to_rgb(2 * v % 1, 0.5 * v + 0.5, abs(0.5 * v - 0.5) + 0.5)
+
+
+def calculateValuePieceOfColor(inColor, outColor, numberOfPiece, countAllPieces, value):
+    if (inColor > outColor):
+        return numberOfPiece - countAllPieces * value
+    else:
+        return countAllPieces * value - numberOfPiece + 1
+
+
+def inte(fromColor, toColor, numberOfPiece, countAllPieces, value):
+    rFrom, gFrom, bFrom = fromColor
+    rTo, gTo, bTo = toColor
+    if (rFrom != rTo):
+        r = calculateValuePieceOfColor(rFrom, rTo, numberOfPiece, countAllPieces, value)
+    else:
+        r = rFrom
+    if (gFrom != gTo):
+        g = calculateValuePieceOfColor(gFrom, gTo, numberOfPiece, countAllPieces, value)
+    else:
+        g = gFrom
+    if (bFrom != bTo):
+        b = calculateValuePieceOfColor(bFrom, bTo, numberOfPiece, countAllPieces, value)
+    else:
+        b = bFrom
+    return r, g, b
 
 
 if __name__ == '__main__':
