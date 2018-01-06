7e66061c49caf98f853753081ed39f6628b48415
jaroslaw <jarek.skrzypczak@o2.pl>
2018-01-06 16:44:11 +0100

    Del measure statistic Refactor

--- GenderRecognition/GenderRecognizer.py
+++ GenderRecognition/GenderRecognizer.py
@@ -1,73 +1,46 @@
 from __future__ import division
-from pylab import *
-from numpy import *
-from scipy import *
-from ipywidgets import *
-import math as mt
-import soundfile
+
 import sys
+import numpy
 import scipy.signal
-import glob
+import soundfile
+from scipy import *
 
 
 def main():
     thresh = 700
-    for shift in range(0, 100, 10):
-        maleCount = 0
-        femaleCount = 0
-        maleHit = 0
-        femaleHit = 0
-        maleFreqSum = 0
-        femaleFreqSum = 0
-        print("Częstotliwość rozróżniania = ", 700 + shift)
-        for name in glob.glob("train/*.wav"):
-            signal, sampleRate = soundfile.read(name, dtype='int16')
-            if (len(signal.shape) == 2):
-                signal = [s[0] for s in signal]
-            w = 1
-            signal = signal[::w]  # co w-ty element
+    signal, sample_rate = soundfile.read(sys.argv[1], dtype='int16')
+    if len(signal.shape) == 2:
+        signal = [s[0] for s in signal]
 
-            fftSignal = fft(signal)
-            fftSignalHalf = fftSignal[1:int(len(fftSignal) / 2)]
-            fftSignalAmpl = abs(fftSignalHalf) * 2 / len(signal)
+    signal = signal[::1]
 
-            fftSignalAmpl[:80] = 0
+    fft_signal = fft(signal)
+    fft_signal_half = fft_signal[1:int(len(fft_signal) / 2)]
+    fft_signal_ampl = abs(fft_signal_half) * 2 / len(signal)
 
-            fftSignalAmpl2 = scipy.signal.decimate(fftSignalAmpl, 2, n=5, ftype='iir', zero_phase=True)
-            fftSignalAmpl3 = scipy.signal.decimate(fftSignalAmpl, 3, n=5, ftype='iir', zero_phase=True)
-            fftSignalAmpl4 = scipy.signal.decimate(fftSignalAmpl, 4, n=5, ftype='iir', zero_phase=True)
-            fftSignalAmpl5 = scipy.signal.decimate(fftSignalAmpl, 5, n=5, ftype='iir', zero_phase=True)
+    fft_signal_ampl[:80] = 0
 
-            mulFFTSingalAmpl = [0 for x in range(len(fftSignalAmpl5))]
-            for i in range(len(fftSignalAmpl5)):
-                mulFFTSingalAmpl[i] = fftSignalAmpl[i] * fftSignalAmpl2[i] * fftSignalAmpl3[i] * fftSignalAmpl4[i] * \
-                                      fftSignalAmpl5[i]
+    fft_signal_ampl2 = scipy.signal.decimate(fft_signal_ampl, 2, n=5, ftype='iir', zero_phase=True)
+    fft_signal_ampl3 = scipy.signal.decimate(fft_signal_ampl, 3, n=5, ftype='iir', zero_phase=True)
+    fft_signal_ampl4 = scipy.signal.decimate(fft_signal_ampl, 4, n=5, ftype='iir', zero_phase=True)
+    fft_signal_ampl5 = scipy.signal.decimate(fft_signal_ampl, 5, n=5, ftype='iir', zero_phase=True)
 
-            maxAmpl = max(mulFFTSingalAmpl)
-            maxFreq = 0
+    mul_fft_signal_ampl = [0 for x in range(len(fft_signal_ampl5))]
+    for i in range(len(fft_signal_ampl5)):
+        mul_fft_signal_ampl[i] = fft_signal_ampl[i] * fft_signal_ampl2[i] * fft_signal_ampl3[i] * fft_signal_ampl4[i] * \
+                              fft_signal_ampl5[i]
 
-            for i in range(len(mulFFTSingalAmpl)):
-                if (mulFFTSingalAmpl[i] == maxAmpl):
-                    maxFreq = i
+    max_ampl = max(mul_fft_signal_ampl)
+    max_freq = 0
+    for i in range(len(mul_fft_signal_ampl)):
+        if mul_fft_signal_ampl[i] == max_ampl:
+            max_freq = i
 
-            if (maxFreq > thresh):
-                if "K" in name:
-                    femaleHit += 1
-            else:
-                if "M" in name:
-                    maleHit += 1
-
-            if "M" in name:
-                maleCount += 1
-                maleFreqSum += maxFreq
-            if "K" in name:
-                femaleCount += 1
-                femaleFreqSum += maxFreq
-        print("Ilość mężczyzn : ", maleCount, " rozpoznanych : ", maleHit, " średnio Hz = ", maleFreqSum / maleCount)
-        print("Ilość kobiet : ", femaleCount, " rozpoznanych : ", femaleHit, " średnio Hz = ",
-              femaleFreqSum / femaleCount)
-        print("Częstotliwość rozróżniania = ", 700 + shift, " rozpoznanych ", maleHit + femaleHit, "/",
-              maleCount + femaleCount, " = ", (maleHit + femaleHit) / (maleCount + femaleCount) *100, "%")
+    if max_freq > thresh:
+        print("K")
+    else:
+        print("M")
 
 
 if __name__ == '__main__':
