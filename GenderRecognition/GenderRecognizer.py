7e6af06ffa5d9ac2e5b26d6f20bc14abc337f62a
jaroslaw <jarek.skrzypczak@o2.pl>
2018-01-13 11:10:55 +0100

    End version

--- GenderRecognition/GenderRecognizer.py
+++ GenderRecognition/GenderRecognizer.py
@@ -1,10 +1,13 @@
 from __future__ import division
-
-import sys
-import numpy
-import scipy.signal
-import soundfile
+from pylab import *
+from numpy import *
 from scipy import *
+from ipywidgets import *
+import math as mt
+import soundfile
+import sys
+import scipy.signal
+import glob
 
 def nearest2Power(length):
     pow = 0
@@ -13,43 +16,43 @@ def nearest2Power(length):
         pow+=1
     return number**(pow-1)
 
+
 def main():
-    thresh = 400
-    signal, sample_rate = soundfile.read(sys.argv[1], dtype='int16')
-    if len(signal.shape) == 2:
+    thresh = 460
+    signal, sampleRate = soundfile.read(sys.argv[1], dtype='int16')
+    if (len(signal.shape) == 2):
         signal = [s[0] for s in signal]
+    w = 1
+    signal = signal[::w]  # co w-ty element
 
-    signal = signal[::1]
-
-    signal2 = numpy.zeros(nearest2Power(len(signal)))
+    signal2 = zeros(nearest2Power(len(signal)))
     for i in range(len(signal2)):
         signal2[i] = signal[i]
 
-    fft_signal = fft(signal2)
-    fft_signal_half = fft_signal[1:int(len(fft_signal) / 2)]
-    fft_signal_ampl = abs(fft_signal_half) * 2 / len(signal2)
+    fftSignal = fft(signal2)
+    fftSignalHalf = fftSignal[1:int(len(fftSignal) / 2)]
+    fftSignalAmpl = abs(fftSignalHalf) * 2 / len(signal2)
 
-    fft_signal_ampl[:80] = 0
+    fftSignalAmpl[:80] = 0
 
-    fft_signal_ampl2 = scipy.signal.decimate(fft_signal_ampl, 2, n=5, ftype='iir', zero_phase=True)
-    fft_signal_ampl3 = scipy.signal.decimate(fft_signal_ampl, 3, n=5, ftype='iir', zero_phase=True)
-    fft_signal_ampl4 = scipy.signal.decimate(fft_signal_ampl, 4, n=5, ftype='iir', zero_phase=True)
-    fft_signal_ampl5 = scipy.signal.decimate(fft_signal_ampl, 5, n=5, ftype='iir', zero_phase=True)
+    fftSignalAmpl2 = scipy.signal.decimate(fftSignalAmpl, 2, n=5, ftype='iir', zero_phase=True)
+    fftSignalAmpl3 = scipy.signal.decimate(fftSignalAmpl, 3, n=5, ftype='iir', zero_phase=True)
+    fftSignalAmpl4 = scipy.signal.decimate(fftSignalAmpl, 4, n=5, ftype='iir', zero_phase=True)
+    fftSignalAmpl5 = scipy.signal.decimate(fftSignalAmpl, 5, n=5, ftype='iir', zero_phase=True)
 
-    mul_fft_signal_ampl = numpy.zeros(len(fft_signal_ampl5))
-    for i in range(len(fft_signal_ampl5)):
-        mul_fft_signal_ampl[i] = fft_signal_ampl[i] * fft_signal_ampl2[i] * fft_signal_ampl3[i] * fft_signal_ampl4[i] * \
-                              fft_signal_ampl5[i]
+    mulFFTSingalAmpl = [0 for x in range(len(fftSignalAmpl5))]
+    for i in range(len(fftSignalAmpl5)):
+        mulFFTSingalAmpl[i] = fftSignalAmpl[i] * fftSignalAmpl2[i] * fftSignalAmpl3[i] * fftSignalAmpl4[i] * \
+                              fftSignalAmpl5[i]
 
-    max_ampl = max(mul_fft_signal_ampl)
-    max_freq = 0
-    for i in range(len(mul_fft_signal_ampl)):
-        if mul_fft_signal_ampl[i] == max_ampl:
-            max_freq = i
+    maxAmpl = max(mulFFTSingalAmpl)
+    maxFreq = 0
 
-    print(max_freq)
+    for i in range(len(mulFFTSingalAmpl)):
+        if (mulFFTSingalAmpl[i] == maxAmpl):
+            maxFreq = i
 
-    if max_freq > thresh:
+    if (maxFreq > thresh):
         print("K")
     else:
         print("M")
